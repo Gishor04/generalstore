d8010f8ce4d834e508a831e2bf3eba19a1d9ef4e
Gishor04 <rajangishor04@gmail.com>
2026-07-22 12:14:26 +0530

    fix vercel

--- api/auth/register.ts
+++ api/auth/register.ts
@@ -1,18 +1,49 @@
+import fs from "fs";
+import path from "path";
+import crypto from "crypto";
+
 export default function handler(req: any, res: any) {
 
     if (req.method !== "POST") {
-        return res.status(405).json({
-            message: "Method not allowed"
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    const { name, email, username, password, storeName, pin } = req.body;
+
+    const filePath = path.join(process.cwd(), "data/users.json");
+
+    const users = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+    const exists = users.find((u: any) => u.email === email);
+
+    if (exists) {
+        return res.status(400).json({
+            error: "User already exists"
         });
     }
 
-    const { name, email, password } = req.body;
+    const user = {
+        id: Date.now(),
+        name,
+        email,
+        username,
+        password,
+        storeName,
+        pin,
+        role: "owner"
+    };
+
+    users.push(user);
+
+    fs.writeFileSync(
+        filePath,
+        JSON.stringify(users, null, 2)
+    );
+
+    const token = crypto.randomBytes(32).toString("hex");
 
     return res.status(200).json({
-        message: "Register API working",
-        user: {
-            name,
-            email
-        }
+        token,
+        user
     });
 }
